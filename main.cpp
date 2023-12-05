b33601ed05794313cc8aabf57c1ac1e5e7e0fcc1
Tarek Ziadé <tarek@ziade.org>
2023-12-05 14:34:53 +0100

    display token/sec

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
+#include <chrono>
 
 #include "ctranslate2/translator.h"
 #include "sentencepiece_processor.h"
@@ -47,18 +48,27 @@ int main(int argc, char* argv[]) {
   std::vector<std::vector<std::string>> text;
   text.push_back(pieces);
 
-
   const std::string model_path("models/falcon-text-summarization-quantized");
   const ctranslate2::models::ModelLoader model_loader(model_path);
 
   ctranslate2::Translator translator(model_loader);
+
+  auto start = std::chrono::high_resolution_clock::now();
   auto results = translator.translate_batch(text);
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> duration = end - start;
+
   std::string joinedString = "";
+  int tokens = 0;
+
   for (const auto& token : results[0].output()) {
     joinedString += token;
+    tokens++;
   }
   token2Text(joinedString);
   std::cout << joinedString << std::endl;
+  std::cout << tokens << " tokens generated (" << std::fixed << std::setprecision(2)
+              << (tokens / duration.count()) << " token/s)" << std::endl;
   return 0;
 }
 
